33a6228a6b5657081a77f25b1622d95b50c1bc0e
Aleksandr <49552536+AleksandrAnisenko@users.noreply.github.com>
2019-06-11 10:12:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ function submitForm() {
 	var form = document.getElementById('form'),
 		div = document.createElement("div");
 	div.className = "info";
-	form.innerHTML = "";
-	div.innerHTML = "......";
+	div.innerHTML = document.getElementById('name').value;
 	form.appendChild(div);
+	div.innerHTML = document.getElementById('surname').value;
+	form.appendChild(div);
+	div.innerHTML = document.getElementById('secondname').value;
+	form.appendChild(div);
+
+
 }
 
